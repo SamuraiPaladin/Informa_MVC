e1f73b9c4988caee31fbe83210f1dbfc9ee5a045
David Silva Jr <david_sjr@hotmail.com>
2020-03-16 22:14:13 -0300

    Tela de Modalidades com suas funções.

---
+++ Web/BD/Repository/ModalidadeDAO.cs
@@ -0,0 +1,98 @@
+﻿using Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Web.BD.Interface;
+
+namespace Web.BD.Repository
+{
+    public class ModalidadeDAO : IDAO<Modalidade>
+    {
+        private readonly string stringConexao = ConfigurationManager.ConnectionStrings["DataContext"].ConnectionString;
+        public bool Adicionar(Modalidade entity)
+        {
+            string query = @"INSERT INTO Modalidades(Descricao, TipoModalidade) VALUES(@Descricao, @TipoModalidade);
+                            SELECT @@IDENTITY";
+            return GravarERetornarVerdadeiroOuFalse(entity, query);
+        }
+        public bool Atualizar(Modalidade entityAntigo, Modalidade entityNovo)
+        {
+            string query = @"UPDATE Modalidades SET Descricao = @DescricaoNovo, TipoModalidade = @TipoModalidadeNovo
+                                WHERE Descricao = @DescricaoAntigo AND TipoModalidade = @TipoModalidadeAntigo";
+            using (var con = new SqlConnection(stringConexao))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@DescricaoNovo", entityNovo.Descricao);
+                cmd.Parameters.AddWithValue("@TipoModalidadeNovo", entityNovo.TipoModalidade);
+
+                cmd.Parameters.AddWithValue("@DescricaoAntigo", entityAntigo.Descricao);
+                cmd.Parameters.AddWithValue("@TipoModalidadeAntigo", entityAntigo.TipoModalidade);
+                return cmd.ExecuteNonQuery() > 0 ? true : false;
+            }
+        }
+
+        public bool Deletar(Modalidade entity)
+        {
+            string query = "DELETE FROM Modalidades WHERE Descricao = @Descricao AND TipoModalidade = @TipoModalidade";
+            using (var con = new SqlConnection(stringConexao))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Descricao", entity.Descricao);
+                cmd.Parameters.AddWithValue("@TipoModalidade", entity.TipoModalidade);
+                return cmd.ExecuteNonQuery() > 0 ? true : false;
+            }
+        }
+
+        public IList<Modalidade> Lista()
+        {
+            string query = "SELECT * FROM Modalidades Order by Descricao";
+            IList<Modalidade> lista = new List<Modalidade>();
+            using (var con = new SqlConnection(stringConexao))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.HasRows)
+                {
+                    while (sdr.Read())
+                    {
+                        var model = new Modalidade
+                        {
+                            Ativo = Convert.ToBoolean(sdr["Ativo"]),
+                            Descricao = sdr["Descricao"].ToString(),
+                            TipoModalidade = sdr["TipoModalidade"].ToString()
+                        };
+                        lista.Add(model);
+                    }
+                }
+                return lista;
+            }
+        }
+        public bool VerificarSeJaExiste(Modalidade entity)
+        {
+            string query = @"SELECT 
+                                	count(1) as qtd
+                                FROM 
+                                	Modalidades
+                                WHERE 
+                                	TipoModalidade = @TipoModalidade AND Descricao = @Descricao";
+            return GravarERetornarVerdadeiroOuFalse(entity, query);
+        }
+        private bool GravarERetornarVerdadeiroOuFalse(Modalidade entity, string query)
+        {
+            using (var con = new SqlConnection(stringConexao))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Descricao", entity.Descricao);
+                cmd.Parameters.AddWithValue("@TipoModalidade", entity.TipoModalidade);
+                return Convert.ToInt32(cmd.ExecuteScalar()) > 0 ? true : false;
+            }
+        }
+    }
+}
---
+++ Web/Controllers/ModalidadeController.cs
@@ -0,0 +1,58 @@
+﻿using Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.BD.Interface;
+using Web.BD.Repository;
+
+namespace Web.Controllers
+{
+    public class ModalidadeController : Controller
+    {
+        private readonly IDAO<Modalidade> _service = new ModalidadeDAO();
+        public ActionResult Index()
+        {
+            return View(_service.Lista());
+        }
+        public JsonResult Adicionar(Modalidade modalidade)
+        {
+            if (VerificaSeTemCampoVazioOuNulo(modalidade))
+                return Json("Preenchimento obrigatório");
+            else
+            {
+                if (!_service.VerificarSeJaExiste(modalidade))
+                    return Json(_service.Adicionar(modalidade));
+                else 
+                    return Json(false);
+            }
+        }
+        private static bool VerificaSeTemCampoVazioOuNulo(Modalidade modalidade)
+        {
+            return string.IsNullOrWhiteSpace(modalidade.Descricao) || string.IsNullOrWhiteSpace(modalidade.TipoModalidade);
+        }
+        public JsonResult Editar(Modalidade modalidade, Modalidade modalidadeEditar)
+        {
+            if (VerificaSeTemCampoVazioOuNulo(modalidadeEditar))
+                return Json("Preenchimento obrigatório");
+            else
+            {
+                if (!_service.VerificarSeJaExiste(modalidadeEditar))
+                    return Json(_service.Atualizar(modalidade, modalidadeEditar));
+                else
+                    return Json(false);
+            }
+        }
+        public JsonResult Deletar(Modalidade modalidade)
+        {
+            //if (VerificaSeTemCampoVazioOuNulo(modalidade))
+            //    return Json("Preenchimento obrigatório");
+            //else
+            if (_service.VerificarSeJaExiste(modalidade))
+                return Json(_service.Deletar(modalidade));
+            else
+                return Json(false);
+        }
+    }
+}
