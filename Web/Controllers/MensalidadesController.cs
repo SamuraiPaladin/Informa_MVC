0ddaf5cd978d3bc73e3fceb1817334fba7a2ede2
Diego Souza <diegomagasouza@hotmail.com>
2020-03-24 21:16:36 -0300

    Pagina Mensalidades

---
+++ Web/Controllers/MensalidadesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class MensalidadesController : Controller
+    {
+        // GET: Mensalidades
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Web/Scripts/menu.js
+++ Web/Scripts/menu.js
@@ -44,3 +44,7 @@ $(document).ready(function () {
 
 $(document).ready(function () {
 });
+
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
