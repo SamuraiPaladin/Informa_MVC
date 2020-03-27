464c94fe5c1ead97e2f95fa52cc12b2e4262d306
beyond-kernel <rafanog.linux@gmail.com>
2020-03-27 20:45:58 -0300

    criado backend inicial de Mensalidade

---
+++ Model/Enums/EnumPaymentStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Model.Enums
+{
+    public class EnumPaymentStatus
+    {
+        public enum PaymentStatus
+        {
+            Pago,
+            [Display(Name = "Pago com atraso")]
+            PagoComAtraso, 
+            Vencido,
+            [Display(Name = "Próximo da data de vencimento")]
+            ProximoDaDataDeVencimento,
+            [Display(Name = "Em Haver")]
+            EmHaver
+        };
+    }
+}
