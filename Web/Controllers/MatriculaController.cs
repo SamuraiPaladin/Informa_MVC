2967f2c5bdda9b076656a520412eb9fffea0c8ff
Guilherme Martinelli <guilhermezanarellimartinelli@hotmail.com>
2020-03-31 10:56:15 -0300

    commit

--- Web/Controllers/MatriculaController.cs
+++ Web/Controllers/MatriculaController.cs
@@ -11,90 +11,90 @@ namespace Web.Controllers
 {
     public class MatriculaController : Controller
     {
-        private readonly IDAOMatricula<Matricula> dAO = new MatriculaDAO();
-        private readonly MatriculaDAO dAOMatricula = new MatriculaDAO();
-        public JsonResult BuscaCep(string cep)
-        {
-            var cepDados = ViaCEP.ViaCEPClient.Search(cep);
-            Matricula model = new Matricula
-            {
-                Bairro = cepDados.Neighborhood,
-                CEP = cep,
-                Cidade = cepDados.City,
-                Endereco = cepDados.Street,
-                Estado = cepDados.StateInitials
-            };
-            return Json(model);
-        }
-        public ActionResult Index()
-        {
-            var model = new Matricula
-            {
-                ListaMatricula = dAOMatricula.Lista().ToList(),
-                ListaMatriculaTurma = dAOMatricula.ListaMatriculaTurma().ToList(),
-                DadosMatriculaTurma = new MatriculaTurma()
-            };
-            return View(model);
-        }
-        public JsonResult Adicionar(Matricula entity)
-        {
-            if (VerificaSeTemCampoVazioOuNulo(entity))
-                return Json("Preenchimento obrigatório");
-            else
-            {
+        //private readonly IDAOMatricula<Matricula> dAO = new MatriculaDAO();
+        //private readonly MatriculaDAO dAOMatricula = new MatriculaDAO();
+        //public JsonResult BuscaCep(string cep)
+        //{
+        //    var cepDados = ViaCEP.ViaCEPClient.Search(cep);
+        //    Matricula model = new Matricula
+        //    {
+        //        Bairro = cepDados.Neighborhood,
+        //        CEP = cep,
+        //        Cidade = cepDados.City,
+        //        Endereco = cepDados.Street,
+        //        Estado = cepDados.StateInitials
+        //    };
+        //    return Json(model);
+        //}
+        //public ActionResult Index()
+        //{
+        //    var model = new Matricula
+        //    {
+        //        ListaMatricula = dAOMatricula.Lista().ToList(),
+        //        ListaMatriculaTurma = dAOMatricula.ListaMatriculaTurma().ToList(),
+        //        DadosMatriculaTurma = new MatriculaTurma()
+        //    };
+        //    return View(model);
+        //}
+        //public JsonResult Adicionar(Matricula entity)
+        //{
+        //    if (VerificaSeTemCampoVazioOuNulo(entity))
+        //        return Json("Preenchimento obrigatório");
+        //    else
+        //    {
 
-                if (dAO.VerificarSeJaExiste(entity))
-                {
-                    return Json(false);
-                }
-                else
-                {
-                    return Json(dAO.Adicionar(entity));
-                }
-            }
-        }
+        //        if (dAO.VerificarSeJaExiste(entity))
+        //        {
+        //            return Json(false);
+        //        }
+        //        else
+        //        {
+        //            return Json(dAO.Adicionar(entity));
+        //        }
+        //    }
+        //}
 
 
 
-        private static bool VerificaSeTemCampoVazioOuNulo(Matricula entity)
-        {
-            if (entity.DataNascimento.ToString("dd/MM/yyyy HH:mm:ss") == "01/01/0001 00:00:00" || string.IsNullOrWhiteSpace(entity.Nome) || string.IsNullOrWhiteSpace(entity.CPF) || string.IsNullOrWhiteSpace(entity.RG) || string.IsNullOrWhiteSpace(entity.CEP) || string.IsNullOrWhiteSpace(entity.Endereco)
-                || string.IsNullOrWhiteSpace(entity.Numero) || string.IsNullOrWhiteSpace(entity.Bairro) || string.IsNullOrWhiteSpace(entity.Cidade) || string.IsNullOrWhiteSpace(entity.Estado) || string.IsNullOrWhiteSpace(entity.Telefone) || entity.Array == null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+        //private static bool VerificaSeTemCampoVazioOuNulo(Matricula entity)
+        //{
+        //    if (entity.DataNascimento.ToString("dd/MM/yyyy HH:mm:ss") == "01/01/0001 00:00:00" || string.IsNullOrWhiteSpace(entity.Nome) || string.IsNullOrWhiteSpace(entity.CPF) || string.IsNullOrWhiteSpace(entity.RG) || string.IsNullOrWhiteSpace(entity.CEP) || string.IsNullOrWhiteSpace(entity.Endereco)
+        //        || string.IsNullOrWhiteSpace(entity.Numero) || string.IsNullOrWhiteSpace(entity.Bairro) || string.IsNullOrWhiteSpace(entity.Cidade) || string.IsNullOrWhiteSpace(entity.Estado) || string.IsNullOrWhiteSpace(entity.Telefone) || entity.Array == null)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
 
-        public JsonResult Editar(Matricula entity, Matricula entityEditar)
-        {
-            if (VerificaSeTemCampoVazioOuNulo(entityEditar))
-                return Json("Preenchimento obrigatório");
-            else
-                return Json(dAO.Atualizar(entity, entityEditar));
-        }
-        public JsonResult Deletar(Matricula entity)
-        {
-            dAO.DeletarMatriculaTurma(entity.Id);
-            return Json(dAO.Deletar(entity));
-        }
-        public JsonResult ModalEditar(int id)
-        {
+        //public JsonResult Editar(Matricula entity, Matricula entityEditar)
+        //{
+        //    if (VerificaSeTemCampoVazioOuNulo(entityEditar))
+        //        return Json("Preenchimento obrigatório");
+        //    else
+        //        return Json(dAO.Atualizar(entity, entityEditar));
+        //}
+        //public JsonResult Deletar(Matricula entity)
+        //{
+        //    dAO.DeletarMatriculaTurma(entity.Id);
+        //    return Json(dAO.Deletar(entity));
+        //}
+        //public JsonResult ModalEditar(int id)
+        //{
 
-            var model = new Matricula()
-            {
-                DadosMatriculaTurma = dAO.DadosMatriculaPorId(id),
-                ListaMatriculaTurma = dAO.ListaMatriulaTurmaPorId(id)
-            };
-            ViewBag.Nome = model.DadosMatriculaTurma.Nome;
-            return Json(model);
-        }
-        public JsonResult Tabela(int id)
-        {
-            return Json(dAO.ListaMatriulaTurmaPorId(id), JsonRequestBehavior.AllowGet);
-        }
+        //    var model = new Matricula()
+        //    {
+        //        DadosMatriculaTurma = dAO.DadosMatriculaPorId(id),
+        //        ListaMatriculaTurma = dAO.ListaMatriulaTurmaPorId(id)
+        //    };
+        //    ViewBag.Nome = model.DadosMatriculaTurma.Nome;
+        //    return Json(model);
+        //}
+        //public JsonResult Tabela(int id)
+        //{
+        //    return Json(dAO.ListaMatriulaTurmaPorId(id), JsonRequestBehavior.AllowGet);
+        //}
     }
 }
