d1376d1c0f587fda53bb898eaee2b1364536e9b9
David Silva Jr <david_sjr@hotmail.com>
2020-06-28 17:53:23 -0300

    Tela Consulta de Pagamento

---
+++ Web/BD/Interface/IConsultaPagamentoDAO.cs
@@ -0,0 +1,14 @@
+﻿using Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.BD.Interface
+{
+    public interface IConsultaPagamentoDAO
+    {
+        IList<ConsultaPagamento> ListaDeValores(ConsultaPagamento consulta);
+    }
+}
--- Web/Controllers/ConsultaPagamentoController.cs
+++ Web/Controllers/ConsultaPagamentoController.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using Model.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.BD.Interface;
+using Web.BD.Repository;
 
 namespace Web.Controllers
 {
     public class ConsultaPagamentoController : Controller
     {
+        private readonly IConsultaPagamentoDAO consultaPagamentoDAO = new ConsultaPagamentoDAO();
         // GET: ConsultaPagamento
         public ActionResult Index()
         {
             return View();
+        }
+        public JsonResult ConsultaTabela(ConsultaPagamento consultaPagamento)
+        {
+            if (!VerificaSeTemCampoVazioOuNulo(consultaPagamento))
+            {
+                return Json(consultaPagamentoDAO.ListaDeValores(consultaPagamento));
+            }
+            else
+            {
+                return Json("");
+            }
+
+        }
+
+        private bool VerificaSeTemCampoVazioOuNulo(ConsultaPagamento consultaPagamento)
+        {
+            return  consultaPagamento.DataInicial.ToString() == "01/01/0001 00:00:00" || string.IsNullOrEmpty(consultaPagamento.DataInicial.ToString()) ;
         }
     }
 }
